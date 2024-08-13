0fe13311651208e457058ab580d9cf50d48e8f2e
MertensFlo <fmertens20@gmail.com>
2024-08-13 16:37:43 +0200

    testing how arm install variables are delivered

--- .github/actions/universal/action_build_specific_commands/install_pipeline_specific.sh
+++ .github/actions/universal/action_build_specific_commands/install_pipeline_specific.sh
@@ -5,6 +5,10 @@ ARM_TAR_PATH=$2
 ARM_DIR_PATH=$3
 
 echo "script install pipeline specific"
+echo wget $ARM_PATH
+echo tar -xf $ARM_TAR_PATH
+echo $ARM_DIR_PATH
+
 wget $ARM_PATH
 tar -xf $ARM_TAR_PATH
 echo $ARM_DIR_PATH >> $GITHUB_PATH

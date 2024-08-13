acd53655d509301c905da51df40a9823904a081c
MertensFlo <fmertens20@gmail.com>
2024-08-13 16:55:21 +0200

    testing

--- .github/actions/universal/action_build_specific_commands/install_pipeline_specific.sh
+++ .github/actions/universal/action_build_specific_commands/install_pipeline_specific.sh
@@ -3,12 +3,13 @@
 ARM_PATH=$1
 ARM_TAR_PATH=$2
 ARM_DIR_PATH=$3
+ARM_CONDITION=$4
 
 echo "script install pipeline specific"
 echo wget $ARM_PATH
 echo tar -xf $ARM_TAR_PATH
 echo $ARM_DIR_PATH
 
-wget $ARM_PATH
+wget $ARM_CONDITION $ARM_PATH
 tar -xf $ARM_TAR_PATH
 echo $ARM_DIR_PATH >> $GITHUB_PATH
